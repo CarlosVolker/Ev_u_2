4d0bc94f6340c1a84bda20554cf9dd5728bc628c
CarlosVolker <143527813+CarlosVolker@users.noreply.github.com>
2024-01-04 13:08:37 -0300

    Evaluacion

---
+++ app/src/main/java/cl/cvega/android/evaluacion_u_2/data/modelo/Compra.kt
@@ -0,0 +1,6 @@
+package cl.cvega.android.evaluacion_u_2.data.modelo
+
+data class Compra (
+    val id:String,
+    val descripcion:String
+)
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Evaluacion_U_2"
+include(":app")
+ 
